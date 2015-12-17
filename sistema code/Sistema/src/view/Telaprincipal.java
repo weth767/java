22771d4047a4502e47150c190685b1b5f465c181
Weth <joao_paulo767@hotmail.com>
2015-12-17 12:18:09 -0200

    Nono backup código sistema

---
+++ sistema code/Sistema/src/view/Telaprincipal.java
@@ -0,0 +1,89 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class Telaprincipal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Telaprincipal window = new Telaprincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Telaprincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 951, 842);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 943, 93);
+		frame.getContentPane().add(menuBar);
+		
+		JMenu mnCadastros = new JMenu("Cadastros");
+		menuBar.add(mnCadastros);
+		mnCadastros.setFont(new Font("Arial", Font.PLAIN, 48));
+		
+		JMenuItem menuItemCadastroDeClientes = new JMenuItem("Cadastro de Clientes");
+		mnCadastros.add(menuItemCadastroDeClientes);
+		
+		JMenuItem mntmCadastroDeFornecedores = new JMenuItem("Cadastro de Fornecedores");
+		mnCadastros.add(mntmCadastroDeFornecedores);
+		
+		JMenuItem mntmCadastroDeProdutos = new JMenuItem("Cadastro de Produtos");
+		mnCadastros.add(mntmCadastroDeProdutos);
+		
+		JMenuItem mntmCadastroDeTipos = new JMenuItem("Cadastro de Tipos de Produtos");
+		mnCadastros.add(mntmCadastroDeTipos);
+		
+		JMenu mnMovimentaes = new JMenu("Movimenta\u00E7\u00F5es");
+		mnMovimentaes.setFont(new Font("Arial", Font.PLAIN, 48));
+		menuBar.add(mnMovimentaes);
+		
+		JMenuItem mntmCompras = new JMenuItem("Compras");
+		mnMovimentaes.add(mntmCompras);
+		
+		JMenuItem mntmVendas = new JMenuItem("Vendas");
+		mnMovimentaes.add(mntmVendas);
+		
+		JMenu mnNewMenu = new JMenu("Relat\u00F3rios");
+		mnNewMenu.setFont(new Font("Arial", Font.PLAIN, 48));
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnSair = new JMenu("Sair");
+		mnSair.setFont(new Font("Arial", Font.PLAIN, 48));
+		menuBar.add(mnSair);
+	}
+}
